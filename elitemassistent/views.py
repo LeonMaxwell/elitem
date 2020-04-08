00aebf8fd2b5ff4d10c44be721c1b8a8bafd0ab0
LeonMaxwell <java.leon@mail.ru>
2020-04-08 20:08:37 +0300

    Ассистент и микросервисы v0.1 (Надготовка)  - Добалена возможность отправлять сообщения боту с дальнейшей обработкой  - Добавлена папка core которая будет содержать ядро сайта который будет обрабатывать микросервисы и его данные

--- elitemassistent/views.py
+++ elitemassistent/views.py
@@ -1,5 +1,17 @@
 # Create your views here.
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView
+
+from elitemassistent.core import elnlu
+
+
+class BindWithAssistant(CreateView):
+    def post(self, request, *args, **kwargs):
+        toUser = ''
+        textToBots = request.POST.get('appeal', None)
+        toUser = elnlu.main(textToBots)
+        contaxt = {'text': textToBots, 'toUser': toUser}
+        return render(request, 'elassistent/extinsions/log.html', contaxt)
 
 
 class HomePage(TemplateView):
